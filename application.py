290945ef9c9447e12fc9694d625a7852bebefc71
Hari Kuttivelil <harikuttivelil@gmail.com>
2019-05-31 18:28:48 -0700

    Starting to add RREQ functionality, still need to develop a mature packet structure, but we're getting there slowly.

--- application.py
+++ application.py
@@ -22,20 +22,24 @@ PKT_DIV = "|"
 from sim2net.application import Application
 
 class dsr_msg:
-    def __init__(self, packet=None, src_id=None, dest_id=None, content=None, count=None):
+    def __init__(self, mtype=None, packet=None, src_id=None, dest_id=None, content=None, count=None, path=None, ind=None):
         if packet:
             self.extract_packet(packet)
         else:
-            self.mtype = MSG_TYPE
+            self.mtype = mtype
             self.src = src_id
-            self.dest = dest
+            self.dest = dest_id
             # Standard message, RERR
             if self.mtype == MSG_TYPE or self.mtype == RERR_TYPE:
                 self.message = content
                 self.count = count
             # RREQ, RREP
-            if self.mtype == RREQ_TYPE or self.mtype == RREP_TYPE:
+            #if self.mtype == RREQ_TYPE or self.mtype == RREP_TYPE:
+            if path is not None:
                 self.path = path
+            else:
+                self.path = []
+        self._zip_packet()
             
     def extract_packet(self, pkt):
         contents = pkt.split(PKT_DIV)
@@ -55,7 +59,9 @@ class dsr_msg:
         if self.mtype == MSG_TYPE or self.mtype == RERR_TYPE:
             self.packet = PKT_DIV.join([str(self.mtype), str(self.src), str(self.dest), self.message])
         if self.mtype == RREQ_TYPE or self.mtype == RREP_TYPE:
-            self.packet = PKT_DIV.join([str(self.mtype), str(self.src), str(self.dest), *self.path])
+            params = [str(self.mtype), str(self.src), str(self.dest)] + \
+                [str(node) for node in self.path]
+            self.packet = PKT_DIV.join(params)
     
 
 class HelloWorld(Application):
@@ -75,6 +81,8 @@ class HelloWorld(Application):
         self.__node_id = node_id
         self.routing_table = {}
         self.routing_lifetimes = {}
+        self.neighbors = []
+        self.communication = None
         print '[node %d] initialize' % self.__node_id
 
     def finalize(self, shared):
@@ -94,12 +102,17 @@ class HelloWorld(Application):
         """
         This method is called at each simulation step.
         """
+        # Update neighbors
+        self.neighbors = neighbors
+        self.communication = communication
         #print("main for", self.__node_id)
-        #if self.__node_id == 0 and time[0] == 1:
-        if self.__node_id == 0:
+        if self.__node_id == 0 and time[0] == 1:
+        #if self.__node_id == 0:
             print("S: %d->ALL" % self.__node_id)
-            communication.send('hi')
+            #communication.send('hi')
+            self.dispatch_msg(self.__node_id, 6, "hi")
         while True:
+            #print ("%d's neighbors:" % self.__node_id, neighbors)
             msg = communication.receive()
             if msg is None:
                 break
@@ -107,6 +120,16 @@ class HelloWorld(Application):
                    % (self.__node_id, msg[0], msg[1]))
             
     def dispatch_msg(self, src_id, dest_id, message):
-        
+        # If cached route is available
+        if dest_id in self.routing_table:
+            # Then send it along that path
+            pass
+        # If we need to find a route
+        else:
+            print ("Need path from %d to %d" % (src_id, dest_id))
+            rreq = dsr_msg(mtype=RREQ_TYPE, src_id=src_id, dest_id=dest_id)
+            self.communication.send(rreq.packet)
+        pass
     
-    def handle_msg(self, 
+    def handle_msg(self, packet):
+        pass
